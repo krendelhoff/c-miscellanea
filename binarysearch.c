6c1abaa01d54eb2e54898aa14e4ae822a20943a0
Savely <forwmrty@gmail.com>
2019-12-23 12:03:19 +0300

    privet ya iz internet

--- binarysearch.c
+++ binarysearch.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #define MAX 100000
-int find(int x, int s[], int n);
+int binsearch(int x, int s[], int n);
+int binsearchv(int x, int s[], int n);
 int main(void)
 {
     int arr[MAX];
     for (int i = 0; i < MAX; i++)
         arr[i] = i;
-    printf("%d\n", find(20, arr, 21));
+    printf("%d\n", binsearch(20, arr, 21));
+    printf("%d\n", binsearchv(20, arr, 21));
     return 0;
 }
-int find(int x, int s[], int n)
+int binsearch(int x, int s[], int n)
 {
     int high = --n, low = 0, mid;
     while (low <= high)
@@ -27,4 +29,16 @@ int find(int x, int s[], int n)
             return mid;
     }
     return -1;
+}
+int binsearchv(int x, int s[], int n)
+{
+    int low = 0, high = --n, mid;
+    while(low < high) 
+    {
+        mid = (low + high) / 2;
+        if (x <= s[low]) 
+            high = mid;
+        else low = mid + 1;
+    }
+    return (x == s[low]) ? low : -1;
 }
