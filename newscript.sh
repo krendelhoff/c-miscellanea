ff674a407e498e9e3114bab499bb8222f3767a01
savely <forwmrty@gmail.com>
2020-01-14 23:11:41 +0300

    colemak ++-++-++

---
+++ newscript.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+echo "sosi"
+counter=0
+while [[ $counter -lt 10 ]]
+do
+  echo "lol"; echo "lol2"
+  counter=$(($counter + 1))
+done
---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("ththt");
+let a = 5;
+var b = 6;
+var c = 7;
+c = a + b;
+console.log(c);
