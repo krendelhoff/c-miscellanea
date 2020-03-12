186dda1163837218b28d5dae3572569eecfd8e10
Savely <krendelhoff1@yandex.ru>
2020-03-12 15:57:10 +0300

    Git

---
+++ C/main.c
@@ -0,0 +1,18 @@
+#include "lib/algorithms.h"
+#include "lib/myio.h"
+#include "lib/mymath.h"
+
+int main(void)
+{
+	int m;
+	int mas[100];
+	int length;
+
+	if (5 > 6)
+		print_int(5);
+	else
+		print_int(6);
+	print_float(89.0095, 4);
+	ft_putchar('\n');
+	return (0);
+}
